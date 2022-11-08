15a789d7b75b985c736684a272cd97bdf6dce9f9
ponsAoki <aokishogo20pk@gmail.com>
2022-11-09 01:35:01 +0900

    ✨性格データや能力値初期値を暫定的にconstantsで定義

---
+++ src/constants/status-values.ts
@@ -0,0 +1,6 @@
+export const STATUS_VALUES = {
+  BASE_STATS: [100, 100, 100, 100, 100, 100],
+  INDIVIDUAL_VALUE: [31, 31, 31, 31, 31, 31],
+  EFFORT_VALUE: [0, 0, 0, 0, 0, 0],
+  REAL_VALUE: [0, 0, 0, 0, 0, 0],
+};
