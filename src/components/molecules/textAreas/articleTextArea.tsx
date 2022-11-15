9ac59d244ed914be732ee3a694cc901a616e7f8b
ponsAoki <aokishogo20pk@gmail.com>
2022-11-16 02:24:17 +0900

    articleTextAreaデザイン修正

--- src/components/molecules/textAreas/articleTextArea.tsx
+++ src/components/molecules/textAreas/articleTextArea.tsx
@@ -1,6 +1,7 @@
 import { Underline } from "@tiptap/extension-underline";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import styles from "../../../styles/articleTextArea.module.css";
 import { ArticleEditBar } from "./articleEditBar";
 
 export const ArticleTextArea = (): JSX.Element => {
@@ -10,9 +11,9 @@ export const ArticleTextArea = (): JSX.Element => {
   });
   if (!editor) return <></>;
   return (
-    <>
+    <div className={styles.textEditor}>
       <ArticleEditBar editor={editor} />
       <EditorContent editor={editor} />
-    </>
+    </div>
   );
 };
